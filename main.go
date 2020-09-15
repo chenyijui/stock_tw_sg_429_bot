c326fcdfdcf555aea2f9349b588d17e1c37af6fa
unknown <a25237780a@gmail.com>
2020-09-16 01:40:34 +0800

    [update] add stock api

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	BotAPI "github.com/go-telegram-bot-api/telegram-bot-api"
+	"io/ioutil"
 	"log"
+	"net/http"
 	Common "stock_tw_sg_429_bot/common"
+	"strings"
 )
 
 func main() {
-	bot, err := BotAPI.NewBotAPI(Common.BOT_TOKEN)
+	bot, err := BotAPI.NewBotAPI(Common.BotToken)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -28,7 +32,22 @@ func main() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
+		split := strings.Split(update.Message.Text, "/chart")
+		fmt.Print(split[1])
+		stockNum := strings.Replace(split[1], " ", "", -1)
+		API := "https://s.yimg.com/nb/tw_stock_frontend/scripts/StxChart/StxChart.9d11dfe155.html?sid=" + stockNum
+		res, err := http.Get(API)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s, _ := ioutil.ReadAll(res.Body)
+
+		fmt.Print(string(s))
+		update.Message.Text = string(s)
 		msg := BotAPI.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+		//BotAPI.NewPhotoUpload()
 		//msg.ReplyToMessageID = update.Message.MessageID
 
 		bot.Send(msg)
