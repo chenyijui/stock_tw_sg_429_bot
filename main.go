63f1cd6f554d461af926dc323e6e1b8f0412252d
unknown <a25237780a@gmail.com>
2020-09-17 00:47:57 +0800

    [update] add photo

--- main.go
+++ main.go
@@ -50,8 +50,6 @@ func main() {
 		update.Message.Text = string(s)
 		//msg := BotAPI.NewMessage(update.Message.Chat.ID, update.Message.Text)
 
-
-
 		// start get photo
 
 		var buf []byte
@@ -67,38 +65,25 @@ func main() {
 		ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
 		defer cancel()
 
-		// navigate to a page, wait for an element, click
-		var example string
 		cdpErr := chromedp.Run(ctx,
-			//访问打开必应页面
-			chromedp.Navigate(`https://cn.bing.com/?mkt=zh-CN`),
-			// 等待右下角图标加载完成
-			chromedp.WaitVisible(`#sh_cp_in`),
-			//搜索框内输入zhangguanzhang
-			chromedp.SendKeys(`#sb_form_q`, `zhangguanzhang`, chromedp.ByID),
-			// 点击搜索图标
-			chromedp.Click(`#sb_form_go`, chromedp.NodeVisible),
-			// 获取第一个搜索结构的超链接
-			chromedp.Text(`#b_results > li:nth-child(2) > div > div > cite`, &example),
+			chromedp.EmulateViewport(560, 360, chromedp.EmulateScale(1)),
+			chromedp.Navigate(`https://s.yimg.com/nb/tw_stock_frontend/scripts/StxChart/StxChart.9d11dfe155.html?sid=2330`),
 			chromedp.CaptureScreenshot(&buf),
 		)
 		if cdpErr != nil {
 			log.Fatal(cdpErr)
 		}
-		if err := ioutil.WriteFile("fullScreenshot.png", buf, 0644); err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("example: %s", example)
+
+		b := BotAPI.FileBytes{Name: "image.jpg", Bytes: buf}
+
 
 		//BotAPI.NewPhotoUpload()
 		//msg.ReplyToMessageID = update.Message.MessageID
 
-
-		upload := BotAPI.NewPhotoUpload(update.Message.Chat.ID, "fullScreenshot.png")
+		upload := BotAPI.NewPhotoUpload(update.Message.Chat.ID, b)
 
 
 		bot.Send(upload)
-
 
 	}
 }
